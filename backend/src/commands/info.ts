591ae0d584ad4bdcf9b192c731fb73a180dd7725
BuduschiyHokage <kolya.skiba.2013@gmail.com>
2022-10-20 01:10:30 +0300

    remove whitelist

--- backend/src/commands/info.ts
+++ backend/src/commands/info.ts
@@ -4,17 +4,12 @@ import { createLoading } from '@/utils'
 
 import { getLastTrades, getCoronaPrice } from '@/services/info'
 
-const WHITELIST = [2073130256, 256640207, 182301431, 869727093]
-
 console.log('info')
 
 // `info` - prints out info about asser for last 24 hours
 composer.command('info', async (ctx) => {
   console.log('info handler')
-  if (!WHITELIST.includes(ctx.from.id)) {
-    ctx.reply('Not Auth')
-    return
-  }
+
   const answer = await createLoading(ctx)
   const ans = await getLastTrades()
   const a = await getCoronaPrice()
